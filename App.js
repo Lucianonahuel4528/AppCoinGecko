122ae8542c0f33fe23461bedd5a399923bb6b046
Luciano <lucianonahuel4528@gmail.com>
2023-03-19 22:14:16 -0300

    mi practica coingecko

---
+++ App.js
@@ -0,0 +1,87 @@
+import { View, Text, FlatList,StyleSheet,TextInput,StatusBar } from 'react-native'
+import React,{useEffect, useState} from 'react'
+import CoinItem from './components/CoinItem'
+
+const App = () => {
+
+  const [coins, setCoins]=useState([])
+  const [search, setSearch]=useState('')
+  const [refreshing, setRefreshing]=useState(false)
+  const loadData = async () =>{
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      );
+     const data = await res.json()
+     setCoins(data)
+  }
+  useEffect(() =>{
+    loadData()
+    
+  },[])
+
+  const styles = StyleSheet.create({
+    container:{
+      backgroundColor: '#141414',
+      alignItems:'center',
+      flex:1
+    },
+    title:{
+      color:'#fff',
+      marginTop:10,
+      fontSize:20
+    },
+    list:{
+      width:'90%',
+      
+    },
+    header:{
+      flexDirection:'row',
+      justifyContent: 'space-between',
+      width:'90%',
+      marginBottom:10
+    },
+    searchInput:{
+      color:'#fff',
+      borderBottomColor:'#4657CE',
+      borderBottomWidth:1,
+      width: '40%',
+      textAlign: 'center'
+    }
+  })
+
+  return (
+    <View style={styles.container}>
+      {/* <StatusBar backgroundColor="#141414"/> */}
+      <View style={styles.header}>
+      <Text style={styles.title}>CryptoMarket</Text>
+      <TextInput style={styles.searchInput}
+        placeholder="Search a coin"
+        placeholderTextColor="#858585"
+        onChangeText={text => setSearch(text)}
+      />
+      </View>
+  
+      <FlatList
+        style={styles.list}
+        data={coins.filter(coin => coin.name.toLowerCase().includes(search.toLocaleLowerCase()) || coin.symbol.toLowerCase().includes(search.toLocaleLowerCase()))}
+        renderItem={({item})=>{
+          return <CoinItem coin={item}/>
+        }}
+        showsVerticalScrollIndicator={false}
+        refreshing={refreshing}
+        onRefresh={ async ()=>{
+          setRefreshing(true)
+          await loadData()
+          setRefreshing(false)
+        }}
+      />
+    </View>
+    
+    
+  )
+}
+
+
+
+
+export default App
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AppCoinGecko
+
+Esta aplicación consume los datos de un API que nos brinda información en tiempo real de los precios de criptomonedas, la misma cuenta con un buscador que realiza un filtro de acuerdo 
+
+al tipo de moneda que quiera consultar el usuario 
